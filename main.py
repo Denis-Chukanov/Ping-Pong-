f5b68a3ef732dc30ed28639990456dad6c929260
Denis-Chukanov <denyabooblik@gmail.com>
2023-09-03 19:22:30 +0300

    timer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 import pygame
-import time
+from time import time
 pygame.init()
 
 win_width, win_height = 700, 500
@@ -51,8 +51,10 @@ class Player(GameSprite):
         if k[down] and self.rect.y <= 400:
             self.rect.y += 5
 
+start_time = time()
+
 ball_pic = pygame.image.load('мяч.png')
-ball = Ball(325, 212, 50, 50, ball_pic, 4, 4, 3)
+ball = Ball(125, 212, 50, 50, ball_pic, 4, 4, 3)
 
 player_pic = pygame.image.load('raketka.png')
 player1 = Player(0, 212, 70, 100, player_pic, 4)
@@ -62,25 +64,52 @@ player1_score = 0
 player2_score = 0
 
 font = pygame.font.SysFont('Arial', 30)
-screen = 'game'
+screen = 'menu'
 game = True
 while game:
+    if screen == 'menu':
+        new_lb =font.render ("Натисніть ENTER щоб почати", True, (255,0,0)) 
+        window.blit(new_lb,(105,200))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                screen = 'game'
+    if screen == 'pause':
+        new_lb =font.render ("Натисніть ENTER щоб продовжити", True, (255,0,0)) 
+        window.blit(new_lb,(105,200))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                screen = 'game'
     if screen == 'game':
+        window.blit(back,(0,0))
         score = font.render('Score ' + str(player1_score) + ':' + str(player2_score), True, (255,0,0)) 
         window.blit(score,(300,0)) 
-        window.blit(back,(0,0))
         ball.update()
         ball.move()
         player1.update()
         player1.move(pygame.K_w, pygame.K_s)
         player2.update()
         player2.move(pygame.K_p, pygame.K_l)
+        timer = int(time() - start_time)
+        game_time = font.render("Час:" + str(timer), True, (0,250,0))
+        window.blit(game_time,(0,0))
         if ball.colide(player1) or ball.colide(player2):
             ball.x_form *= -1
         if ball.rect.x >= 700:
             player1_score += 1
+            player1 = Player(0, 212, 70, 100, player_pic, 4)
+            player2 = Player(630, 212, 70, 100, player_pic, 4)
+            ball = Ball(175, 212, 50, 50, ball_pic, 4, 4, 3)
+            screen = 'pause'
         if ball.rect.x <= 0:
             player2_score += 1
+            player1 = Player(0, 212, 70, 100, player_pic, 4)
+            player2 = Player(630, 212, 70, 100, player_pic, 4)
+            ball = Ball(525, 212, 50, 50, ball_pic, 4, -4, 3)
+            screen = 'pause'
         if player1_score == 3:
             screen = "over1"
         if player2_score == 3:
@@ -103,6 +132,8 @@ while game:
                 player1 = Player(0, 212, 70, 100, player_pic, 4)
                 player2 = Player(630, 212, 70, 100, player_pic, 4)
                 ball = Ball(325, 212, 50, 50, ball_pic, 4, 4, 3)
+                player1_score = 0
+                player2_score = 0
                 screen = 'game'
 
     if screen == 'over2':
@@ -119,7 +150,11 @@ while game:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                 player1 = Player(0, 212, 70, 100, player_pic, 4)
                 player2 = Player(630, 212, 70, 100, player_pic, 4)
-                ball = Ball(325, 212, 50, 50, ball_pic, 4, 4, 3)
+                ball = Ball(125, 212, 50, 50, ball_pic, 4, 4, 3)
+                player1_score = 0
+                player2_score = 0
+                game_time = 0
+                start_time = time()
                 screen = 'game'
 
     pygame.display.update()
